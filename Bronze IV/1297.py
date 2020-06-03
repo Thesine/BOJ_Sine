742754772d124db27116f64fa71852d1eb0603ed
Thesine <64006353+Thesine@users.noreply.github.com>
2020-06-03 17:36:30 +0900

    Update 1297.py

--- Bronze IV/1297.py
+++ Bronze IV/1297.py
@@ -1,7 +1,7 @@
 import math
 a, b, c = map(int, input().split())
-x = math.sqrt(a*a//((b*b)+(c*c))*b*b)
-x = math.floor(x)
-y = math.sqrt(a*a//((b*b)+(c*c))*c*c)
-y = math.floor(y)
+x = math.sqrt(a*a/((b*b)+(c*c))*b*b)
+x = math.trunc(x)
+y = math.sqrt(a*a/((b*b)+(c*c))*c*c)
+y = math.trunc(y)
 print(x,y)
