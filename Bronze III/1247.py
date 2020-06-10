8c51423e4d62b1ad98970c9492c6a40254676c14
Thesine <64006353+Thesine@users.noreply.github.com>
2020-06-10 15:28:53 +0900

    Create 1247.py

---
+++ Bronze III/1247.py
@@ -0,0 +1,22 @@
+def cal():
+  n = int(input())
+  s = 0
+  for i in range(n):
+    a = int(input())
+    s = s + a
+    if i == n-1:
+      if s==0:
+        print(0)
+        break
+      elif s>0:
+        print('+')
+        break
+      else:
+        print('-')
+        break
+    else:
+      pass
+
+cal()
+cal()
+cal()
