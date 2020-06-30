2fc56dd50bbe43fe9ef50226aa68b0560264fd78
Thesine <64006353+Thesine@users.noreply.github.com>
2020-06-30 10:25:03 +0900

    Delete 3003.py

--- 3003.py
+++
@@ -1,5 +0,0 @@
-king, queen, rook, bishop, knight, pawn = map(int, input().split())
-
-original_king, original_queen, original_rook, original_bishop, original_knight, original_pawn = 1, 1, 2, 2, 2, 8
-
-print(original_king-king, original_queen-queen, original_rook-rook, original_bishop-bishop, original_knight-knight, original_pawn-pawn)
